1a044e99f59a9cdc7f0bc94d7cf20c6078155826
Joachim Dorel <joachim.dorel@me.com>
2024-01-03 14:02:23 +0100

    Added React Router to manage the routes for the front-end app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
 import './App.css';
 
 import SignUpForm from './components/SignUpForm';
+import LoginForm from './components/LoginForm';
+import Home from './components/Home';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="">
-      <SignUpForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </Router>
   );
 }
 
